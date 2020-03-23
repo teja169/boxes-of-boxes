32c8f5c562904ab8abb61177cb86e47ce0b71881
teja169 <60430122+teja169@users.noreply.github.com>
2020-03-23 16:54:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+ 
+var engine;
+var world;
+var boxes = [];
+ 
+var ground;
+
+
+ 
+function setup() {
+    createCanvas(400, 300);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+ 
+    
+    var options = {
+        isStatic: true
+    }
+    ground = Bodies.rectangle(200,height-4, width, 10, options);
+    World.add(world, ground);
+}
+ 
+function mousePressed() {
+    if (mouseY < 350) {
+        boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
+    }
+}
+ 
+function draw() {
+    background(21, 228, 239);
+   
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+    noStroke();
+    fill(170);
+    strokeWeight(4);
+    rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, width, 10);
+    fill(255);
+    textSize(15); 
+   
+}
+ 
+function Box(x, y, w, h, options) {
+    var options = {
+        friction: 0.5,
+        restitution: 0.5,
+    }
+ 
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+ 
+    this.show = function () {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+ 
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(1);
+        stroke(255);
+        fill(127);
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
+}
